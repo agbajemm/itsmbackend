e2e76107607fea8aa692aed1847e4f62ed6c00bb
agbajemm <agbajemhmd316@gmail.com>
2025-11-17 10:14:32 +0100

    build

---
+++ documentchecker/Migrations/20251117091422_AddManageEngineRequestsTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace documentchecker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddManageEngineRequestsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ManageEngineRequests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    TechnicianName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JsonData = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManageEngineRequests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ManageEngineRequests");
+        }
+    }
+}
